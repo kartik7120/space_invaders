f233b6df02ef5d6aafdf0e085b19880a24f889aa
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-08-16 17:09:23 +0530

    Added logic in NewLazer function to move the image of lazer at the center of the ship

--- player/laser.go
+++ player/laser.go
@@ -2,10 +2,12 @@ package player
 
 import (
 	"game/utils"
-	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+const (
+	lazerSpeed = 350
 )
 
 var lazerImage *ebiten.Image = utils.MustLoadImage("lazer.png")
@@ -15,6 +17,14 @@ type Lazer struct {
 }
 
 func NewLazer(x, y float64) *Lazer {
+
+	bounds := lazerImage.Bounds()
+	halfWidth := float64(bounds.Dx()) / 2
+	halfHeight := float64(bounds.Dy()) / 2
+
+	x = x - halfWidth
+	y = y - halfHeight
+
 	return &Lazer{
 		X: x,
 		Y: y,
@@ -23,25 +33,20 @@ func NewLazer(x, y float64) *Lazer {
 
 func (l *Lazer) Update(player *Player) {
 
-	if l.Y < 0 {
-		lazerImage.Deallocate()
-	}
-	// l.Coliision(player.game.invaders)
-	l.X = player.position.x
-	l.Y -= 5
+	speed := lazerSpeed / ebiten.TPS()
+
+	l.Y -= float64(speed)
 }
 
 func (l *Lazer) Draw(screen *ebiten.Image) {
-	vector.DrawFilledRect(lazerImage, float32(l.X), float32(l.Y), 2, 5, color.White, true)
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(l.X, l.Y)
+	screen.DrawImage(lazerImage, op)
 }
 
-func (l *Lazer) Coliision(invaders [][]*Invader) {
-	for i := 0; i < len(invaders); i++ {
-		for j := 0; j < len(invaders[i]); j++ {
-			if invaders[i][j].position.x < l.X && l.X < invaders[i][j].position.x+float64(invaders[i][j].invader.Bounds().Dx()) && invaders[i][j].position.y < l.Y && l.Y < invaders[i][j].position.y+float64(invaders[i][j].invader.Bounds().Dy()) {
-				invaders[i][j] = nil
-				l.Y = -1
-			}
-		}
-	}
+func (l *Lazer) Collider() utils.Rect {
+	bounds := lazerImage.Bounds()
+
+	return utils.NewRect(l.X, l.Y, float64(bounds.Dx()), float64(bounds.Dy()))
 }
