d5231c9faf60c37b7d072e309d56e5f04ba72890
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-08-30 13:13:20 +0530

    Added must load font functions for space invader and open sans font

--- utils/utils.go
+++ utils/utils.go
@@ -1,11 +1,22 @@
 package utils
 
 import (
+	"bytes"
+	_ "embed"
 	"image"
 	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
+
+//go:embed OpenSans-Regular.ttf
+var englishTTF []byte
+
+var EnglighFileSource *text.GoTextFaceSource
+
+//go:embed ElectronPulse.ttf
+var spaceInvaderTTF []byte
 
 func MustLoadImage(name string) *ebiten.Image {
 	f, err := os.Open(name)
@@ -20,4 +31,24 @@ func MustLoadImage(name string) *ebiten.Image {
 	}
 
 	return ebiten.NewImageFromImage(img)
+}
+
+func MustLoadFont() *text.GoTextFaceSource {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(englishTTF))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
+func MustLoadSpaceInvaderFont() *text.GoTextFaceSource {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(spaceInvaderTTF))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
 }
