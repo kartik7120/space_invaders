51f2b8348820bf19f9703883b2f7faaf60d10dd6
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-08-30 21:48:20 +0530

    added text and isKeyPressed function to change back to title screen

--- scenes/GameOverScreen.go
+++ scenes/GameOverScreen.go
@@ -18,6 +18,9 @@ var GameOverScreen *manager.Scene = &manager.Scene{
 	Update: func(setReady func()) error {
 		// this has to be called to guarantee that we don't call Update before Draw
 		setReady()
+		if ebiten.IsKeyPressed(ebiten.KeyEnter) {
+			Context.Manager.SwitchTo("title")
+		}
 		return nil
 	},
 	Draw: func(screen *ebiten.Image) {
@@ -28,8 +31,17 @@ var GameOverScreen *manager.Scene = &manager.Scene{
 			Size:   20,
 		}, float64(2))
 
+		textWidth2, textHeight2 := text.Measure("Press Enter to go back to the title screen", &text.GoTextFace{
+			Source: SpaceInvaderTextSource,
+			Size:   10,
+		}, float64(2))
+
 		op := &text.DrawOptions{}
+		op2 := &text.DrawOptions{}
+
 		op.GeoM.Translate(float64(320/2)-float64(textWidth/2), float64(240/2)-float64(textHeight))
+		op2.GeoM.Translate(float64(320/2)-float64(textWidth2/2), float64(240/2)-float64(textHeight2)+float64(20))
 		text.Draw(screen, "Game Over", &text.GoTextFace{Source: SpaceInvaderTextSource, Size: 20}, op)
+		text.Draw(screen, "Press Enter to go back to the title screen", &text.GoTextFace{Source: SpaceInvaderTextSource, Size: 10}, op2)
 	},
 }
