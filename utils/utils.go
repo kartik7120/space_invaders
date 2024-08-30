8ca3023d72681933d2dfa71b92feef552b491178
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-08-30 21:34:30 +0530

    Added a function to load invaders font and embed in the go binary

--- utils/utils.go
+++ utils/utils.go
@@ -18,6 +18,9 @@ var EnglighFileSource *text.GoTextFaceSource
 //go:embed ElectronPulse.ttf
 var spaceInvaderTTF []byte
 
+//go:embed Invaders.ttf
+var InvaderTTF []byte
+
 func MustLoadImage(name string) *ebiten.Image {
 	f, err := os.Open(name)
 	if err != nil {
@@ -45,6 +48,16 @@ func MustLoadFont() *text.GoTextFaceSource {
 
 func MustLoadSpaceInvaderFont() *text.GoTextFaceSource {
 	s, err := text.NewGoTextFaceSource(bytes.NewReader(spaceInvaderTTF))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
+func MustLoadInvaderFont() *text.GoTextFaceSource {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(InvaderTTF))
 
 	if err != nil {
 		panic(err)
