c15bf8630303eb39b165fc50e3b2f4aaeeabcc06
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-09-07 08:41:23 +0530

    Added texts to show when the game has ended

--- scenes/EndScreen.go
+++ scenes/EndScreen.go
@@ -1,8 +1,11 @@
 package scenes
 
 import (
+	"game/player"
+	"strconv"
+
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	manager "github.com/tducasse/ebiten-manager"
 )
 
@@ -15,11 +18,46 @@ var EndScreen *manager.Scene = &manager.Scene{
 		// this has to be called to guarantee that we don't call Update before Draw
 		setReady()
 		if ebiten.IsKeyPressed(ebiten.KeyControl) {
-
+			Context.Manager.SwitchTo("title")
 		}
 		return nil
 	},
 	Draw: func(screen *ebiten.Image) {
-		ebitenutil.DebugPrint(screen, "Game Over! Press Ctrl to restart")
+
+		op := &text.DrawOptions{}
+		textWidth, textHeight := text.Measure("Press Ctrl to restart the game", &text.GoTextFace{
+			Source: englishTextSource,
+			Size:   15,
+		}, float64(2))
+		op.GeoM.Translate(float64(320/2)-float64(textWidth/2), float64(240/2)-float64(textHeight))
+		text.Draw(screen, "Press Ctrl to restart the game", &text.GoTextFace{
+			Source: englishTextSource,
+			Size:   15,
+		}, op)
+
+		titleWidth, titleHeight := text.Measure("Thank you for playing!", &text.GoTextFace{
+			Source: spaceInvaderTextSource,
+			Size:   20,
+		}, float64(2))
+
+		op2 := &text.DrawOptions{}
+		op2.GeoM.Translate(float64(320/2)-float64(titleWidth/2), float64(240/2)-float64(titleHeight)-float64(textHeight))
+		text.Draw(screen, "Thank you for playing!", &text.GoTextFace{
+			Source: spaceInvaderTextSource,
+			Size:   20,
+		}, op2)
+
+		scoreWidth, scoreHeight := text.Measure("Your score was: "+strconv.Itoa(player.GetGameState("lvl3").Score), &text.GoTextFace{
+			Source: englishTextSource,
+			Size:   15,
+		}, float64(2))
+
+		op3 := &text.DrawOptions{}
+		op3.GeoM.Translate(float64(320/2)-float64(scoreWidth/2), float64(240/2)-float64(scoreHeight)-float64(titleHeight)-float64(textHeight))
+		text.Draw(screen, "Your score was: "+strconv.Itoa(player.GetGameState("lvl3").Score), &text.GoTextFace{
+			Source: englishTextSource,
+			Size:   15,
+		}, op3)
+
 	},
 }
